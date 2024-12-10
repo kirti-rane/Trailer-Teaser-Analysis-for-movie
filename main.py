94832a5c45a07710f4d7a8e950d5c403ca672412
Kirti Rane <kirtirane@Kirtis-MacBook-Pro.local>
2024-12-10 13:06:16 +0530

    filepath

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from correlation import video_correlation
 
 if __name__ == "__main__":
 
-    teaser_path = "Videos/GK_teaser.mp4"
-    trailer_path = "Videos/GK_trailer.mp4"
-    embeddings_path = "/Users/kirtirane/Downloads/new_data_celebrity_embeddings.npy"
+    teaser_path = "Videos/Pathaan | Official Teaser | Shah Rukh Khan | Deepika Padukone | John Abraham | Siddharth Anand - YRF (1080p, h264, youtube).mp4"
+    trailer_path = "Videos/Pathaan Trailer | Shah Rukh Khan | Deepika Padukone | John Abraham | Siddharth A | YRF Spy Universe - YRF (1080p, h264, youtube).mp4"
+    embeddings_path = "Trained_model/new_data_celebrity_embeddings.npy"
     output_dir = "./output_frames"
 
 
-    # detect_celebrities_in_video(teaser_path, trailer_path, embeddings_path,output_dir)
+    detect_celebrities_in_video(teaser_path, trailer_path, embeddings_path,output_dir)
 
-    # trailer_duration = get_video_duration(trailer_path)
-    # if trailer_duration:
-    #     print(f"Trailer Video Duration: {trailer_duration:.2f} seconds")
+    trailer_duration = get_video_duration(trailer_path)
+    if trailer_duration:
+        print(f"Trailer Video Duration: {trailer_duration:.2f} seconds")
 
-    # teaser_duration = get_video_duration(teaser_path)
-    # if teaser_duration:
-    #     print(f"Teaser Video Duration: {teaser_duration:.2f} seconds")
+    teaser_duration = get_video_duration(teaser_path)
+    if teaser_duration:
+        print(f"Teaser Video Duration: {teaser_duration:.2f} seconds")
 
     video_correlation(teaser_path,trailer_path)
 
